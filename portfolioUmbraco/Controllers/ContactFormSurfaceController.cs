2fd3ce0919291b8929b54f432a9530b41ad18aa4
DESKTOP-6QQE44J\TThorsager <t-thorsager@homail.com>
2018-03-24 16:31:41 +0100

    first upload

---
+++ portfolioUmbraco/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,45 @@
+﻿using portfolioUmbraco.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Core.Models;
+
+namespace portfolioUmbraco.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            TempData["success"] = true;
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+            comment.SetValue("messageName", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("messageContent", model.Message);
+            comment.SetValue("umbracoNaviHide", true);
+
+            //save
+            Services.ContentService.Save(comment);
+
+            
+
+            return RedirectToCurrentUmbracoPage();
+
+        }
+    }
+}
---
+++ portfolioUmbraco/ViewModels/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace portfolioUmbraco.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Remember to enter your name, please")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Remember to enter your email address, please")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$", ErrorMessage = "Enter a valid email, please")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Remember to enter a subject, please")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Remember to enter your message, please")]
+        public string Message { get; set; }
+
+    }
+}
